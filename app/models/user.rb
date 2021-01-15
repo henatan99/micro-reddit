4e91bd2f909f32f783a4e2424222d9ad175af4be
Henok <henatan99@gmail.com>
2021-01-15 19:23:24 +0300

    Fix linter errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
-    has_many :posts, dependent: :destroy
-    has_many :comments, through: :posts
+  has_many :posts, dependent: :destroy
+  has_many :comments, through: :posts
 
-    validates :username, presence: true, uniqueness:true, length: { minimum: 4,  maximum: 10}
-    validates :email, presence: true, uniqueness:true
+  validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 10 }
+  validates :email, presence: true, uniqueness: true
 end
