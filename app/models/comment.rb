637f68636864d236e8583463bba7441e0f337635
Your Name <basaijapascal9@gmail.com>
2021-01-18 18:13:11 +0300

    add validation to comment data model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  validates :commenter, presence: true
+  validates :body, presence: true, length: { minimum: 2, maximum: 50 }
 end
